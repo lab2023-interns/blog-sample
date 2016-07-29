5245d29ebfedf4da181fc1cdba27201f80051a92
BerkanAcar <berkanacar93@gmail.com>
2016-07-29 15:51:29 +0300

    profile update message error fixed

--- app/controllers/user/profile_controller.rb
+++ app/controllers/user/profile_controller.rb
@@ -14,7 +14,7 @@ class User::ProfileController < User::UserApplicationController
 
   def update
     @profile.update(profile_params)
-    respond_with([:user, @profile], location: user_profile_path)
+    respond_with(:user, @profile, location: user_profile_path)
   end
 
   private
